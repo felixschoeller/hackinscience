95ee7756a6fe13604d310efb853b684c4dd22808
Felix Schoeller <felix.schoeller@gmail.com>
2015-09-21 16:51:13 +0200

    h

--- exercises/014/solution.py
+++ exercises/014/solution.py
@@ -1,6 +1,5 @@
 import sys
-print(sys.argv[0])
-if (len(sys.argv) == 0):
+if (len(sys.argv) == 1):
     print("usage: python3 solution.py PARAM")
 else:
-    print(sys.argv[0])
+    print(sys.argv[1])
